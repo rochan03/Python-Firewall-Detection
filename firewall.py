c9ffd5e6b9a5cb70cda1794c3112cc29189e71d4
Rochan Aminzadeh <146111476+rochan03@users.noreply.github.com>
2024-10-15 17:59:33 -0700

    Create firewall.py

---
+++ firewall.py
@@ -0,0 +1,49 @@
+
+#necessary imports in order for the script to work
+from scapy.all import sniff, IP, TCP, UDP
+
+#main function that make sure the firewall function works
+def firewall():
+
+    #This could be changed in your liking (I allowed all IPS)
+    allowed_ips = []
+
+    #This could be changed in your liking
+    blocked_ips = ['192.168.0.216']
+
+
+    #List of blocked Ports - you can change this
+    allowed_ports = [80, 445, 5353]
+
+    def log_to_terminal(message):
+        print(message)
+
+
+    # function to check traffic in each packet
+    def check_traffic(packet):
+        if packet.haslayer(IP):
+            source_ip = packet[IP].src
+
+            #check if the packet is TCP OR UDP
+            if packet.haslayer(TCP) or packet.haslayer(UDP):
+                destination_port = packet[TCP].dport if packet.haslayer(TCP) else packet[UDP].dport
+
+                #Check if the destination port is not in the allowed port list
+                if destination_port not in allowed_ports:
+                     log_to_terminal(f"port: {destination_port} is not allowed")
+                     return
+                log_to_terminal(f"allowded traffic from {source_ip}")
+
+    #This captures packets and applies the check_traffic function
+    sniff(prn = check_traffic, filter = "ip", store = 0)
+
+
+if __name__ == "__main__":
+    firewall()
+
+
+
+
+
+
+
